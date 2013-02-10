5bb5becc1f9c9a609f76135a0e0e34025bb917e9
kaprasanna <kaprasannagt@gmail.com>
2013-02-10 17:57:32 +0530

    Initial commit

---
+++ derma-images-model/src/com/bh/derma/images/model/ISeries.java
@@ -0,0 +1,27 @@
+package com.bh.derma.images.model;
+
+import java.util.List;
+
+public interface ISeries {
+
+	public String getSeriesID();
+
+	public void setSeriesID(String seriesID);
+
+	public String getName();
+
+	public void setName(String name);
+
+	public String getNotes();
+
+	public void setNotes(String notes);
+
+	public List<Object> getPhotos();
+
+	public void setPhotos(List<Object> photos);
+	
+	public IStudy getParentStudy();
+	
+	public void setParentStudy(IStudy parentStudy);
+
+}
